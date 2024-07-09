3748dcc599f8de5e169fc8390e6fe70ed301d3bc
CristobalPinha <cpina@utem.cl>
2024-07-09 00:08:33 -0400

    Co-authored-by: BENJAMÍN VICENTE FERNÁNDEZ TOLEDO <bfernandezt@utem.cl> Co-authored-by: BASTIÁN ALONSO PIZARRO PACHECO <bpizarrop@utem.cl>

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,10 @@
 
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Empleado, Prestamo
+from .models import Empleado, Prestamo, Cuota
 from .forms import EmpleadoForm, PrestamoForm
 from django.urls import reverse
-
+from datetime import timedelta
+from django.utils import timezone
 # Crea tus views aqui.
 
 def registrar_empleado(request):
@@ -40,20 +41,41 @@ def eliminar_empleado(request, RUT_empleado):
 #==============
 
 def crear_prestamo(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PrestamoForm(request.POST)
         if form.is_valid():
-            prestamo = form.save()  # Guarda el formulario y obtiene la instancia del préstamo
-            return redirect(reverse('detalle_prestamo', args=[prestamo.id_prestamo]))  # Redirecciona a la vista de detalle
+            prestamo = form.save()  # Guardar el préstamo para poder asociar cuotas
+            
+            monto_cuota = prestamo.monto_pagar / prestamo.cantidad_cuotas
+            fecha_emision = timezone.now()  # Asumiendo que la fecha de emisión es la fecha actual
+            
+            for i in range(1, prestamo.cantidad_cuotas + 1):
+                fecha_vencimiento = fecha_emision + timedelta(days=30*i)  # Asumiendo cuotas mensuales
+                cuota = Cuota(
+                    Prestamo=prestamo,
+                    numero_cuota=i,
+                    monto_cuota=monto_cuota,
+                    cuota_fecha_emision=fecha_emision,
+                    cuota_fecha_vencimiento=fecha_vencimiento,
+                    cuota_fecha_pago=None  # Inicialmente None, se actualizará cuando se pague la cuota
+                )
+                cuota.save()
+            
+            return redirect('listar_prestamo')  # Asegúrate de tener esta ruta definida en urls.py
     else:
         form = PrestamoForm()
     return render(request, 'crear_prestamo.html', {'form': form})
 
 def detalle_prestamo(request, id_prestamo):
     prestamo = Prestamo.objects.get(id_prestamo=id_prestamo)  # Asegúrate de que 'id' es el nombre correcto del campo en tu modelo Prestamo
-    cuotas = prestamo.cuotas.all()  # Asume que tienes una relación que permite acceder a 'cuotas' desde un objeto 'prestamo'
+    cuotas = prestamo.cuotas.all()  # Accede a las cuotas a través de la instancia de prestamo
     return render(request, 'detalle_prestamo.html', {'prestamo': prestamo, 'cuotas': cuotas})
 
 def listar_prestamo(request):
     prestamos = Prestamo.objects.all()
     return render(request, 'listar_prestamo.html', {'prestamos': prestamos})
+
+def eliminar_prestamo(request, id_prestamo):
+    prestamo = Prestamo.objects.get(id_prestamo=id_prestamo)
+    prestamo.delete()
+    return redirect('listar_prestamo')
